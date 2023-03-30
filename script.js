340a3e112ae81900f210180c4c6f57f3101b021a
DeborahLyra <deborahp.lyra@gmail.com>
2023-03-30 17:49:07 -0300

    Iniciado projeto do portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+function calculeSuaConta() {
+
+    const valorConta = 100
+
+    const valorComGorgeta = valorConta * 0.1 + valorConta
+
+    console.log(valorComGorgeta)
+}
+
+calculeSuaConta()
